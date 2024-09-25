9ae18f48963d6de99ba1ba8146b6e5aba11da244
Jubibani <strawberryloli3@gmail.com>
2024-09-25 20:34:22 +0800

    added screen 1 and screen 2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cc17-3k-orpilla-christopher-act5
 An Android app that can display artwork or family photos. Include at least 5 images with their respective descriptions and authors
+[]()
---
+++ app/src/main/java/com/example/artspaceapp/MainActivity2.kt
@@ -0,0 +1,34 @@
+package com.example.artspaceapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+
+        val backButton : Button = findViewById(R.id.button)
+        val nextButton : Button = findViewById(R.id.button2)
+
+
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        nextButton.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+
+        }
+    }
+
+}
